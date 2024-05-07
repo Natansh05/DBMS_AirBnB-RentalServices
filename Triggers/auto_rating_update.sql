a5c766e0aa3a33b2c5bc1eed659b39f80f2d6863
natansh05 <202201445@daiict.ac.in>
2024-05-07 17:22:22 +0530

    Finally updated auto_rating_update.sql

--- Triggers/auto_rating_update.sql
+++ Triggers/auto_rating_update.sql
@@ -1,40 +1,38 @@
-
-create or replace function rating1() returns trigger as $auto_rating_update$
+create or replace function rating() returns trigger as $auto_rating_update$
 begin
-	create or replace view rating_propertywise as 
-	select property_id , r.rating
-	from ratings as r;
-	----------(1)
-	
 	
 	create or replace view final11 as 
-	select property_id,avg(rating) as rat_avg
-	from rating_propertywise
-	group by property_id;
+	select property_id,avg(rating) as rating_avg
+	from ratings
+	group by property_id
+	order by property_id asc;
 	
 	
 	update host
 	set avg_rating = (
-		select avg(rat_avg)
+		select avg(rating_avg)
 		from (
-			select host_id,rat_avg from property natural join final11 order by host_id ASC
+			select host_id,rating_avg from property natural join final11 order by host_id ASC
 		) as r1
 		where host.host_id = r1.host_id 
 	);
-
+	
+	raise notice 'BHAI FUNCTION CHAL RAHA HAI';
+	
 	return new;
 	
 end;
 $auto_rating_update$ language plpgsql;
 
--- CALL rating1();
-
-
+-- CREATION OF TRIGGER
 create trigger auto_rating_update after insert on ratings
-for each row execute procedure rating1();
+for each row execute procedure rating();
 
 insert into ratings (user_id,property_id,rating,reviews) values
-('USR0000037','1000000009',3.9,'Staff Service could have been better');
+('USR0000037', '1000000009',3.9,' Staff Service could have been better');
 
 
 SELECT * FROM information_schema.triggers WHERE event_object_table = 'ratings'; -- Check the trigger definition
+
+-- select * from ratings
+-- select * from host      ---to check whether entry is there in both or not
